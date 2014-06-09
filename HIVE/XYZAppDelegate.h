f2a2c91a432f8cd7c72267628612a48319991b1b
Mason <mdimarco3@gatech.edu>
2014-06-08 18:05:05 -0700

    initial commit

---
+++ HIVE/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  HIVE
+//
+//  Created by Mason Dimarco on 6/8/14.
+//  Copyright (c) 2014 Mason Dimarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HIVE/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HIVE
+//
+//  Created by Mason Dimarco on 6/8/14.
+//  Copyright (c) 2014 Mason Dimarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
