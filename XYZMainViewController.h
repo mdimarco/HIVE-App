5e5ddf6bc481d2d2d2271a41203bc2cf69907094
Mason <mdimarco3@gatech.edu>
2014-06-09 22:30:00 -0700

    Added view controllers for main screen of storyboard

---
+++ XYZMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZMainViewController.h
+//  HIVE
+//
+//  Created by Mason Dimarco on 6/9/14.
+//  Copyright (c) 2014 Mason Dimarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZMainViewController : UIViewController
+
+@end
