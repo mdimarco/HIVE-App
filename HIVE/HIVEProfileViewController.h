cdf290adc1f8a64f5c4f595b903a6a1e712e3599
Mason <mdimarco3@gatech.edu>
2014-06-15 13:54:50 -0700

    Profile & Register storyboard & view controllers

---
+++ HIVE/HIVEProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  HIVEProfileViewController.h
+//  HIVE
+//
+//  Created by Mason Dimarco on 6/15/14.
+//  Copyright (c) 2014 Mason Dimarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIVEProfileViewController : UIViewController
+
+@end
---
+++ HIVE/HIVERegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  HIVERegisterViewController.h
+//  HIVE
+//
+//  Created by Mason Dimarco on 6/15/14.
+//  Copyright (c) 2014 Mason Dimarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIVERegisterViewController : UIViewController
+
+@end
